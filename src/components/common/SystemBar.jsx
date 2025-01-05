543fe25f41b7689bf32204bf7cc90944216c0e79
ml0328 <minjeelyu111100@gmail.com>
2025-01-05 17:39:42 +0900

    공용 컴포넌트 세팅 설정

--- src/components/common/SystemBar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ReactComponent as SystemBarIcon } from '../../assets/icons/systembar/Systembar_black.svg';
-
-const SystemBar = () => {
-  return <SystemBarIcon style={{ width: '100%', height: 'auto' }} />;
-};
-
-export default SystemBar;
