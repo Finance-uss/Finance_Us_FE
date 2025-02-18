4ca9ec29c2d3d0834af28ea4a386d016418c6e13
anit7215 <tina112703@gmail.com>
2025-02-18 12:56:24 +0900

    test:배포 테스트

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   base: "./",
   server: {
     proxy: {
       '/api': {
-        target: 'http://13.209.210.46:8080', // 백엔드 서버
-        changeOrigin: true, // 요청 헤더의 Origin을 타겟 서버로 설정
-        rewrite: (path) => path.replace(/^\/api/, ''), // /api를 제거하여 백엔드 엔드포인트로 전달
+        target: 'http://13.209.210.46:8080',  // 백엔드 서버의 URL이 HTTP라도 프록시 설정이 필요
+        changeOrigin: true,
+        secure: false,  // HTTP 서버로 요청을 보낼 수 있도록 설정
+        rewrite: (path) => path.replace(/^\/api/, ''),  // '/api' 제거
       },
     },
-  },
-  build: {
-    outDir: "dist", 
   },
 });
