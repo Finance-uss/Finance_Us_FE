869e80d63f902d9b79180b1ac0b5b55b327e740d
anit7215 <tina112703@gmail.com>
2025-02-18 12:30:02 +0900

    test:배포테스트

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "./",
   server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://13.209.210.46:8080',  // HTTPS URL로 변경
-        changeOrigin: true,
-        secure: true, // HTTPS로 요청이 처리되도록 설정
-      }
-    }
+    port: 5173, 
   },
   build: {
-    outDir: "dist",
+    outDir: "dist", 
   },
 });
