19dfc385a95d455b2d517d606142b56d56e2ce92
anit7215 <tina112703@gmail.com>
2025-02-18 12:46:09 +0900

    test:배포테스트

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ export default defineConfig({
   plugins: [react()],
   base: "./",
   server: {
-    port: 5173, 
+    proxy: {
+      '/api': {
+        target: 'http://13.209.210.46:8080', // 백엔드 서버
+        changeOrigin: true, // 요청 헤더의 Origin을 타겟 서버로 설정
+        rewrite: (path) => path.replace(/^\/api/, ''), // /api를 제거하여 백엔드 엔드포인트로 전달
+      },
+    },
   },
   build: {
     outDir: "dist", 
