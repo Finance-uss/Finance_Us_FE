10394ecb4911926dab06c3eafdab76d294d72cf6
anit7215 <tina112703@gmail.com>
2025-02-18 12:14:39 +0900

    test:배포 테스트

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,14 @@ export default defineConfig({
   plugins: [react()],
   base: "./",
   server: {
-    port: 5173, 
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,  // .env 파일의 VITE_API_URL 변수를 사용
+        changeOrigin: true,
+        secure: false, // 서버가 HTTPS가 아니라면 `false`로 설정
+      }
+    }
   },
   build: {
     outDir: "dist", 
